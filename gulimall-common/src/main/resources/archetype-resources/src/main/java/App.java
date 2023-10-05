63e7021441c2ce1265893c205d64060f26ac9fe7
QingxiHu <470544301h@gmail.com>
2023-10-05 20:47:09 +0800

    p24

---
+++ gulimall-common/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.xxg.eureka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/feign/CouponFeignService.java
@@ -0,0 +1,18 @@
+package com.atguigu.gulimall.member.feign;
+
+import com.atguigu.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Administrator
+ * @Description
+ * @create 2023-10-05 16:11
+ */
+
+@FeignClient("gulimall-coupon")
+public interface CouponFeignService {
+
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
